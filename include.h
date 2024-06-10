fd090bd4b1dd4f64345d9c4f6e81904734953880
cyucccx <167344133+cyucccx@users.noreply.github.com>
2024-06-11 00:02:34 +0800

    Add files via upload

--- include.h
+++ include.h
@@ -7,9 +7,26 @@
 #include <stdlib.h>
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
+#include <SDL2/SDL_ttf.h>
+
+extern SDL_Window* window;
+extern SDL_Renderer* screen;
+
+typedef struct
+{
+    double x,y;
+    int width,height;
+    SDL_Texture *texture;
+}Player;
+
+void create_player(Player play, SDL_Renderer *render, SDL_Surface *image, SDL_Rect a);
+
+int SDL_no_choice_one_character(char *background_image, char *character_image, char *character_message, char *character_name);
+
+int SDL_choice_one_character(char *background_image, char *character_image,char *sellect_button_message_up,char *sellect_button_message_middle,char *sellect_button_message_down);
+
 
 typedef struct scene{
-    int32_t number;
     char *name;
     char *background;
     char *backpack_icon;
@@ -41,6 +58,28 @@ typedef struct reply{
     int32_t change_favor3;
 }sReply;
 
+typedef struct final_reply{
+    char *option_box;
+    char *object1;
+    char *option1;
+    char *good_next1;
+    char *bad_next1;
+    int32_t change_favor1;
+    int32_t favor_threshold1;
+    char *object2;
+    char *option2;
+    char *good_next2;
+    char *bad_next2;
+    int32_t change_favor2;
+    int32_t favor_threshold2;
+    char *object3;
+    char *option3;
+    char *good_next3;
+    char *bad_next3;
+    int32_t change_favor3;
+    int32_t favor_threshold3;
+}sFinalReply;
+
 typedef struct backpack{
     int32_t items_number;
     char *description_box;
@@ -55,115 +94,22 @@ typedef struct character{
     int32_t favor;
 }sCharacter;
 
-void allocate_scene(sScene *input){
-    input->name = calloc(100, sizeof(sScene));
-    input->background = calloc(100, sizeof(sScene));
-    input->backpack_icon = calloc(100, sizeof(sScene));
-    input->character_number = 0;
-    input->dialogue = 0;
-    input->reply = 0;
-    input->backpack = 0;
-}
+void allocate_scene(sScene *input);
 
-void free_scene(sScene *input){
-    free(input->name);
-    input->name = NULL;
-    free(input->background);
-    input->background = NULL;
-    free(input->backpack_icon);
-    input->backpack_icon = NULL;
-}
+void free_scene(sScene *input);
 
-void allocate_dialogue(sDialogue *input){
-    input->dialog_box = calloc(100, sizeof(char));
-    input->string_number = 0;
-    input->speaker = calloc(100, sizeof(char *));
-    input->text = calloc(100, sizeof(char *));
-    input->next = calloc(100, sizeof(char *));
-    for (int32_t i = 0; i < 100; i++){
-        input->speaker[i] = calloc(100, sizeof(char));
-        input->text[i] = calloc(100, sizeof(char));
-    }
-}
+void allocate_dialogue(sDialogue *input);
 
-void free_dialogue(sDialogue *input){
-    free(input->dialog_box);
-    input->dialog_box = NULL;
-    free(input->next);
-    input->next = NULL;
-    for (int32_t i = 0; i < 100; i++){
-        free(input->speaker[i]);
-        input->speaker[i] = NULL;
-        free(input->text[i]);
-        input->text[i] = NULL;
-    }
-    free(input->speaker);
-    input->speaker = NULL;
-    free(input->text);
-    input->text = NULL;
-}
+void free_dialogue(sDialogue *input);
 
-void allocate_reply(sReply *input){
-    input->option_box = calloc(100, sizeof(char));
-    input->object = calloc(100, sizeof(char));
-    input->option1 = calloc(100, sizeof(char));
-    input->next1 = calloc(100, sizeof(char));
-    input->change_favor1 = 0;
-    input->option2 = calloc(100, sizeof(char));
-    input->next2 = calloc(100, sizeof(char));
-    input->change_favor2 = 0;
-    input->option3 = calloc(100, sizeof(char));
-    input->next3 = calloc(100, sizeof(char));
-    input->change_favor3 = 0;
-}
+void allocate_reply(sReply *input);
 
-void free_reply(sReply *input){
-    free(input->option_box);
-    input->option_box = NULL;
-    free(input->object);
-    input->object = NULL;
-    free(input->option1);
-    input->option1 = NULL;
-    free(input->next1);
-    input->next1 = NULL;
-    free(input->option2);
-    input->option2 = NULL;
-    free(input->next2);
-    input->next2 = NULL;
-    free(input->option3);
-    input->option3 = NULL;
-    free(input->next3);
-    input->next3 = NULL;
-}
+void free_reply(sReply *input);
 
-void allocate_backpack(sBackpack *input){
-    input->items_number = 0;
-    input->description_box = calloc(100, sizeof(char));
-    input->name = calloc(9, sizeof(char *));
-    input->photo = calloc(9, sizeof(char *));
-    input->description = calloc(9, sizeof(char *));
-    for (int32_t i = 0; i < 9; i++){
-        input->name[i] = calloc(100, sizeof(char));
-        input->photo[i] = calloc(100, sizeof(char));
-        input->description = calloc(100, sizeof(char));
-    }
-}
+void allocate_backpack(sBackpack *input);
 
-void free_backpack(sBackpack *input){
-    free(input->description_box);
-    input->description_box = NULL;
-    for (int32_t i = 0; i < 9; i++){
-        free(input->name[i]);
-        input->name[i] = NULL;
-        free(input->photo[i]);
-        input->photo[i] = NULL;
-        free(input->description[i]);
-        input->description[i] = NULL;
-    }
-    free(input->name);
-    input->name = NULL;
-    free(input->photo);
-    input->photo = NULL;
-    free(input->description);
-    input->description = NULL;
-}
+void free_backpack(sBackpack *input);
+
+void allocate_FinalReply(sFinalReply *input);
+
+void free_FinalReply(sFinalReply *input);
