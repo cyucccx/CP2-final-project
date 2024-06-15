2eea9cf809879bd7f8a65f418455bf73d215bcd6
cyucccx <167344133+cyucccx@users.noreply.github.com>
2024-06-15 17:54:19 +0800

    Add files via upload

--- main.c
+++ main.c
@@ -34,6 +34,8 @@ int main(){
     int32_t home = 0;
     int32_t select_character = 0;
     sSelect select;
+    char myname[100] = {0};
+    char myavatar[100] = {0};
     char *home_background = 0;
     char *home_button = 0;
     int32_t max_character = 0; // create an array to store the favor of every character
@@ -213,6 +215,32 @@ int main(){
                 select.avatar5 = calloc(100, sizeof(char));
                 sscanf(buffer, "avatar5 = \"%[^\"]\"", select.avatar5);
                 printf("%s\n", select.avatar5);
+                int end; // SDL
+                if (end == 11){
+                    strcpy(myname, select.name1);
+                    strcpy(myavatar, select.avatar1);
+                    // free select
+                }
+                else if (end == 12){
+                    strcpy(myname, select.name2);
+                    strcpy(myavatar, select.avatar2);
+                    // free select
+                }
+                else if (end == 13){
+                    strcpy(myname, select.name3);
+                    strcpy(myavatar, select.avatar3);
+                    // free select
+                }
+                else if (end == 14){
+                    strcpy(myname, select.name4);
+                    strcpy(myavatar, select.avatar4);
+                    // free select
+                }
+                else if (end == 15){
+                    strcpy(myname, select.name5);
+                    strcpy(myavatar, select.avatar5);
+                    // free select
+                }
             }
         }
 
@@ -300,6 +328,9 @@ int main(){
                     character_index = 0;
                     char *temp = 0;
                     getstring(buffer, &temp);
+                    if (strcmp(temp, "me") == 0){
+                        strncpy(temp, myname, strlen(myname));
+                    }
                     for (int32_t i = 0; i < count_character; i++){
                         if (strcmp(character[i].name, temp) == 0){
                             check = 1;
@@ -327,6 +358,9 @@ int main(){
                 }
                 else if (strstr(buffer, "avatar") != 0){
                     getstring(buffer, &character[character_index].avatar);
+                    if (strcmp(character[character_index].avatar, "me_avatar") == 0){
+                        strncpy(character[character_index].avatar, myavatar, strlen(myavatar));
+                    }
                     if (check == 0){
                         if (strcmp(character[character_index].avatar, "null") == 0){
                             // switch to no photo function
@@ -419,17 +453,14 @@ int main(){
                     if (buffer[i] >= 48 && buffer[i] <= 57){
                         if (buffer[i+2] >= 48 && buffer[i+2] <= 57){
                             dialogue.string_number = (buffer[i]-48)*100 + (buffer[i+1]-48)*10 + buffer[i+2]-48;
-                            printf("345\n");
                             break;
                         }
                         else if (buffer[i+1] >= 48 && buffer[i+1] <= 57){
                             dialogue.string_number = (buffer[i]-48)*10 + buffer[i+1]-48;
-                            printf("350\n");
                             break;
                         }
                         else{
                             dialogue.string_number = buffer[i]-48;
-                            printf("355\n");
                             break;
                         }
                     }
@@ -439,6 +470,9 @@ int main(){
             if (string_index < dialogue.string_number){
                 if (strstr(buffer, "speaker") != 0){
                     getstring(buffer, &dialogue.speaker[string_index]);
+                    if (strcmp(dialogue.speaker[string_index], "me") == 0){
+                        strncpy(dialogue.speaker[string_index], myname, strlen(myname));
+                    }
                     printf("%s\n", dialogue.speaker[string_index]);
                 }
                 else if (strstr(buffer, "text") != 0){
